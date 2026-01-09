197e83b8634d633e9a00a2d6f917803f1fb5f712
Leif Fieger <leif.fieger@sunrise.net>
2026-01-09 11:01:31 +0100

    home

---
+++ src/Components/Filter/Filter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './filter.css'
+
+export default function Filter() {
+    return (
+        <div className="filter-container">
+            <h2>Filter Component</h2>
+        </div>
+    )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+import Filter from '../../components/Filter/Filter.jsx'
+
+
+export default function Home() {
+
+  return (
+    <div className="page-content">
+      <h1>Car4You</h1>
+      <Filter />
+    </div>
+  )
+} 
